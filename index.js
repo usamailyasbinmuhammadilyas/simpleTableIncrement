a2e6bbb872f21b68727c9330b3eede2dc7c39a86
usama1234 <136082078+usamailyasbinmuhammadilyas@users.noreply.github.com>
2023-07-29 04:23:59 -0700

    simple table by increment button

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+function tables2(){
+    document.getElementById('he').innerHTML=count='';
+for (let jan = 1;jan < 11; jan++) 
+    {document.getElementById('he').innerHTML += "2  x "+jan + " " + "="+ jan*2 +"<br>" }}
+
+    function tables3(){
+        document.getElementById('he').innerHTML=count='';
+        for (let jan = 1;jan < 11; jan++) 
+            {document.getElementById('he').innerHTML += "3  x "+jan + " " + "="+ jan*3 +"<br>" }}
+
+
+            function tables4(){
+                document.getElementById('he').innerHTML=count='';
+                for (let jan = 1;jan < 11; jan++) 
+                    {document.getElementById('he').innerHTML += "4  x "+jan + " " + "="+ jan*4 +"<br>" }}
+
+                    function tables5(){
+                        document.getElementById('he').innerHTML=count='';
+                        for (let jan = 1;jan < 11; jan++) 
+                            {document.getElementById('he').innerHTML += "5  x "+jan + " " + "="+ jan*5 +"<br>" }}
+
+                            function tables6(){
+                                document.getElementById('he').innerHTML=count='';
+                                for (let jan = 1;jan < 11; jan++) 
+                                    {document.getElementById('he').innerHTML += "6  x "+jan + " " + "="+ jan*6 +"<br>" }}
+
+                                    function tables7(){
+                                        document.getElementById('he').innerHTML=count='';
+                                        for (let jan = 1;jan < 11; jan++) 
+                                            {document.getElementById('he').innerHTML += "7  x "+jan + " " + "="+ jan*7 +"<br>" }}
+
+                                            function tables8(){
+                                                document.getElementById('he').innerHTML=count='';
+                                                for (let jan = 1;jan < 11; jan++) 
+                                                    {document.getElementById('he').innerHTML += "8  x "+jan + " " + "="+ jan*8 +"<br>" }}
+
+                                                    function tables9(){
+                                                        document.getElementById('he').innerHTML=count='';
+                                                        for (let jan = 1;jan < 11; jan++) 
+                                                            {document.getElementById('he').innerHTML += "9  x "+jan + " " + "="+ jan*9 +"<br>" }}
+
+                                                            function tables10(){
+                                                                document.getElementById('he').innerHTML=count='';
+                                                                for (let jan = 1;jan < 11; jan++) 
+                                                                    {document.getElementById('he').innerHTML += "10  x "+jan + " " + "="+ jan*10 +"<br>" }}
